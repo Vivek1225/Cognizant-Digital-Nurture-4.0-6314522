1bbda84431aa5f5b3e651c8d961e3a32bcf85a88
CHVivek7 <vivekch1225@gmail.com>
2025-07-10 15:38:02 +0530

    week-4 ex-2 handson load country

---
+++ week_4_Spring_REST_using_Spring_Boot/2_Handson_4_Load_Country/code/springlearn/src/main/java/com/cognizant/springlearn/CountryService.java
@@ -0,0 +1,36 @@
+package com.cognizant.springlearn;
+
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CountryService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryService.class);
+
+    private Map<String, Country> countries;
+
+    public Map<String, Country> getCountries() {
+        LOGGER.debug("Inside getCountries()");
+        return countries;
+    }
+
+    public void setCountries(Map<String, Country> countries) {
+        LOGGER.debug("Inside setCountries()");
+        this.countries = countries;
+        logAllCountries();
+    }
+
+    public Country getCountryByCode(String code) {
+        LOGGER.debug("Inside getCountryByCode()");
+        Country country = countries.get(code);
+        if (country != null) {
+            LOGGER.info("Selected Country - Code: {}, Name: {}", country.getCode(), country.getName());
+        }
+        return country;
+    }
+
+    private void logAllCountries() {
+        LOGGER.info("All Available Countries:");
+        countries.forEach((code, country) -> LOGGER.info("Code: {}, Name: {}", code, country.getName()));
+    }
+}
